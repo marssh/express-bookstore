14afb6a79f5d10dab1e93255b936c6261e648dc0
diana <diliang4783573@gmail.com>
2020-06-23 17:00:09 -0700

    added tests for PUT, DELETE routes

--- __test__/bookstore.test.js
+++ __test__/bookstore.test.js
@@ -43,21 +43,74 @@ describe('Testing if we can add a book/ validate', () => {
       year: 1999
     })
     expect(response.statusCode).toEqual(201);
-    expect(response.body).toEqual({ book: {
-      isbn: "919191",
-      amazon_url: "https://www.amazon.com",
-      author: "God",
-      language: "en",
-      pages: 456,
-      publisher: "Princeton",
-      title: "Bible",
-      year: 1999
-    }});
+    expect(response.body).toEqual({
+      book: {
+        isbn: "919191",
+        amazon_url: "https://www.amazon.com",
+        author: "God",
+        language: "en",
+        pages: 456,
+        publisher: "Princeton",
+        title: "Bible",
+        year: 1999
+      }
+    });
     const inDB = await request(app).get(`/books/${response.body.book.isbn}`);
     expect(inDB.statusCode).toEqual(200);
     expect(inDB.body.book.isbn).toEqual("919191");
   });
 });
+
+describe("Delete a book", () => {
+  test("test if the delete route deletes a valid isbn", async () => {
+    const response = await request(app).delete('/books/121221212');
+
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toEqual({ message: "Book deleted" });
+
+    const inDB = await request(app).get(`/books/121221212`);
+    expect(inDB.statusCode).toEqual(404);
+  })
+
+  test("test if the delete route throw error with invalid isbn", async () => {
+    const response = await request(app).delete('/books/1');
+
+    expect(response.statusCode).toEqual(404);
+  });
+});
+
+describe("PUT route for a book", () => {
+  test("test if the put route updates a valid book", async () => {
+    const response = await request(app).put('/books/1323323').send({
+      "amazon_url": "https://www.google.com",
+      "author": "pineapple",
+      "language": "en",
+      "pages": 500,
+      "publisher": "fruits",
+      "title": "PUPPIES",
+      "year": 2000
+    });
+
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toEqual({
+      book: {
+        isbn: "1323323",
+        amazon_url: "https://www.google.com",
+        author: "pineapple",
+        language: "en",
+        pages: 500,
+        publisher: "fruits",
+        title: "PUPPIES",
+        year: 2000
+      }
+    })
+
+    const inDB = await request(app).get(`/books/${response.body.book.isbn}`);
+    expect(inDB.statusCode).toEqual(200);
+    expect(inDB.body.book.author).toEqual("pineapple");
+  });
+});
+
 
 
 
